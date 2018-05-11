483399122ae860cffdbaca681c28fa98cb744036
Karl Hoech <karl.hoech@rockwellcollins.com>
2018-05-11 07:41:13 -0500

    Make commands dependent on success of previous commands

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@
 docker build --force-rm=true \
 	-f case-ta6-tools.dockerfile \
 	-t case-ta6-tools:latest \
-	.
+	. && \
 docker build --force-rm=true \
 	-f case-ta6-uxas.dockerfile \
 	-t case-ta6-uxas:latest \
-	.
+	. && \
 docker run \
 	-it \
 	--hostname in-container \
