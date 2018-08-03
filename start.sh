d0fb50a3a36dbb248f0242777c742e6e142e571a
Karl Hoech <karl.hoech@rockwellcollins.com>
2018-08-03 12:21:56 -0500

    Update start script wrt previous commits

--- start.sh
+++ start.sh
@@ -13,10 +13,15 @@ docker build --force-rm=true \
 	-t case-ta6-odroid-xu4-build:latest \
 	. && \
 docker build --force-rm=true \
-	-f case-ta6-uxas.dockerfile \
-	-t case-ta6-uxas:latest \
+	-f case-ta6-uxas-tools.dockerfile \
+	-t case-ta6-uxas-tools:latest \
+	. && \
+docker build --force-rm=true \
+	-f case-ta6-uxas-build.dockerfile \
+	-t case-ta6-uxas-build:latest \
 	. && \
 docker run \
+	--privileged \
 	-it \
 	--hostname in-container \
 	--rm \
